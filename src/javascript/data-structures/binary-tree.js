9b780825dcf89ed5611b4496b8bc283f1242696d
dgyarmati <dpolya.w@gmail.com>
2019-11-12 22:15:09 +0100

    binary search tree insert implementation

---
+++ src/javascript/data-structures/binary-tree.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data) {
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        } else if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+
+}
+
+let node = new Node(1);
+node.insert(0);
+node.insert(3);
+node.insert(-1);
+node.insert(-2);
+console.log(node);
