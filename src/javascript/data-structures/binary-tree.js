0e231dd5d7a2c4e5682a647697cae1b480c8374a
dgyarmati <dpolya.w@gmail.com>
2019-11-13 13:03:55 +0100

    binary search tree validate implementation

--- src/javascript/data-structures/binary-tree.js
+++ src/javascript/data-structures/binary-tree.js
@@ -31,11 +31,40 @@ class Node {
         return null;
     }
 
+    /* goes through all the left nodes first, then the right of the last node, then hits the bottom, returns true,
+        which makes it go back to the previous nod, checks all the right nodes of the left branch up to the root, then
+        goes to the right, doing the same. if a false is ever returned, that cascaded through the stack, and returned.
+
+        the trick is that max will be the value of the left node, min of the right node.
+     */
+    validate(node, min = null, max = null) {
+        if (max !== null && node.data > max) { // left side
+            return false;
+        }
+
+        if (min !== null && node.data < min) { // right side
+            return false;
+        }
+
+        if (node.left && !this.validate(node.left, min, node.data)) { // left side
+            return false;
+        }
+
+        if (node.right && !this.validate(node.right, node.data, max)) { // right side
+            return false;
+        }
+
+        return true;
+    }
+
 }
 
 let node = new Node(1);
 node.insert(0);
 node.insert(3);
 node.insert(-1);
+node.insert(-3);
 node.insert(-2);
+node.right = new Node(-15);
 console.log(node.contains(5));
+console.log(node.validate(node));
