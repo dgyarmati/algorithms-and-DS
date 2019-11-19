7949a0ad387cc785f01ea20ac843e28325734eeb
dgyarmati <dpolya.w@gmail.com>
2019-11-19 13:41:32 +0100

    stack implementation

--- src/java/data-structures/queue/FixedSizeQueue.java
+++ src/java/data-structures/queue/FixedSizeQueue.java
@@ -1,5 +1,6 @@
 import java.util.NoSuchElementException;
 
+// you retrieve the elements in the order they were added
 // remove from the front, insert to the end
 // standard implementation; dequeue doesn't actually remove element, only if it's followed by enqueue
 class FixedSizeQueue {
@@ -26,6 +27,7 @@ class FixedSizeQueue {
     int dequeue() {
         if (!isEmpty()) {
             int item = array[front];
+            array[front] = 0;
             front = (front + 1) % this.capacity;
             currentSize--;
             return item;
@@ -50,5 +52,31 @@ class FixedSizeQueue {
 
     private boolean isEmpty() {
         return currentSize == 0;
+    }
+
+    public void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        FixedSizeQueue q = new FixedSizeQueue(3);
+
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+
+        q.print();
+
+        System.out.println(q.dequeue()); // 1
+        System.out.println(q.dequeue()); // 2
+        System.out.println(q.dequeue()); // 3
+
+        q.print(); // [0, 0, 0]
+
+        q.enqueue(1);
+        q.print(); // [1, 0, 0]
     }
 }
---
+++ src/java/data-structures/stack/Stack.java
@@ -0,0 +1,73 @@
+import java.util.NoSuchElementException;
+
+// you retrieve the elements the opposite order they were added
+class Stack {
+    private int[] arr;
+    private int top;
+    private int capacity;
+
+    Stack(int capacity) {
+        this.arr = new int[capacity];
+        this.capacity = capacity;
+        this.top = -1;
+    }
+
+    public void push(int item) {
+        if (!isFull()) {
+            arr[++top] = item;
+        }
+    }
+
+    public int pop() {
+        if (!isEmpty()) {
+            int item = arr[top];
+            arr[top--] = 0;
+            return item;
+        }
+        throw new NoSuchElementException("Stack is empty");
+    }
+
+    public int peek() {
+        if (!isEmpty()) {
+            return arr[top];
+        }
+        throw new NoSuchElementException("Stack is empty");
+    }
+
+    private boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    private boolean isEmpty() {
+        return top == -1;
+    }
+
+    public int size() {
+        return arr.length;
+    }
+
+    public void print() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack(3);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s.pop()); // 3
+        System.out.println(s.pop()); // 2
+        System.out.println(s.pop()); // 1
+
+        s.print(); // [0, 0, 0]
+
+        s.push(1);
+
+        s.print(); // [1, 0, 0]
+    }
+
+}
