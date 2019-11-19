555998ec0f46d708faf4471803232869498d8c5f
dgyarmati <dpolya.w@gmail.com>
2019-11-19 12:58:46 +0100

    added fixed size queue implementation, removed faulty one

--- src/java/data-structures/queue/FixedSizeQueue.java
+++ src/java/data-structures/queue/FixedSizeQueue.java
@@ -1,56 +1,47 @@
 import java.util.NoSuchElementException;
 
 // remove from the front, insert to the end
+// standard implementation; dequeue doesn't actually remove element, only if it's followed by enqueue
 class FixedSizeQueue {
-    private int[] arr;
-    private int front, rear, currentSize;
-    private int capacity;
+    int front, rear, currentSize;
+    int capacity;
+    int[] array;
 
-    FixedSizeQueue(int size) {
-        this.arr = new int[size];
-        this.capacity = size;
+    FixedSizeQueue(int capacity) {
+        this.array = new int[capacity];
+        this.capacity = capacity;
         this.front = 0;
         this.rear = -1;
         this.currentSize = 0;
     }
 
-    // adds an item to the end of the queue - that is, 0, 1, ...
-    public void enqueue(int item) {
+    void enqueue(int item) {
         if (!isFull()) {
-            // if we previously removed an item, the array's size is now capacity - 1, which would cause the new element to replace the element at 0, so we need to
-            // enlarge the array
-            if (arr.length < capacity) {
-                int[] enlargedArray = enlargeArray(arr, currentSize + 1);
-                arr = enlargedArray;
-            }
-            rear = (rear + 1) % capacity;
-            arr[rear] = item;
+            this.rear = (this.rear + 1) % this.capacity;
+            this.array[this.rear] = item;
             currentSize++;
         }
     }
 
-    private int[] enlargeArray(int[] array, int newSize) {
-        int[] enlargedArray = new int[newSize];
-        for (int i = 1; i < enlargedArray.length; i++) {
-            enlargedArray[i] = array[i - 1];
-        }
-        return enlargedArray;
-    }
-
-    // remove item from queue from front, that is, starting from 0
-    public int dequeue() {
+    int dequeue() {
         if (!isEmpty()) {
-            int frontElement = arr[front];
+            int item = array[front];
+            front = (front + 1) % this.capacity;
             currentSize--;
-
-            // remove first item and shrink array
-            int[] reducedArray = new int[currentSize];
-            System.arraycopy(arr, front + 1, reducedArray, 0, currentSize);
-            arr = reducedArray;
-
-            return frontElement;
+            return item;
         }
         throw new NoSuchElementException("Queue is empty");
+    }
+
+    int peek() {
+        if (!isEmpty()) {
+         return array[front];
+        }
+        throw new NoSuchElementException("Queue is empty");
+    }
+
+    public int size() {
+        return currentSize;
     }
 
     public boolean isFull() {
@@ -59,47 +50,5 @@ class FixedSizeQueue {
 
     public boolean isEmpty() {
         return currentSize == 0;
-    }
-
-    public int size() {
-        return currentSize;
-    }
-
-    public int peek() {
-        if (!isEmpty()) {
-            return arr[front];
-        }
-        throw new NoSuchElementException("Queue is empty");
-    }
-
-    public void print() {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-
-        FixedSizeQueue q = new FixedSizeQueue(3);
-        q.enqueue(2);
-        q.enqueue(3);
-        q.enqueue(5);
-
-        System.out.print("array: ");
-        q.print(); // [2, 3, 5]
-
-        System.out.println(q.peek()); // 2
-
-        int n = q.dequeue(); // 2
-        int n1 = q.dequeue(); // 3
-        System.out.println("last two elements dequeued: " + n + ", " + n1);
-
-        q.print(); // [5]
-        q.enqueue(8);
-
-        System.out.print("array: ");
-        q.print(); // [8, 5]
-
     }
 }
