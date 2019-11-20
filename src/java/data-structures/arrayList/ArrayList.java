3643df42826ec3236386bfae8fc1baed5af6a8ef
dgyarmati <dpolya.w@gmail.com>
2019-11-20 10:40:32 +0100

    refactor

--- src/java/data-structures/arrayList/ArrayList.java
+++ src/java/data-structures/arrayList/ArrayList.java
@@ -1,23 +1,23 @@
 class ArrayList {
     private int capacity;
     private int[] data;
-    private int currentSize;
+    private int currentIndex;
 
     public ArrayList() {
         this.capacity = 5;
         this.data = new int[this.capacity];
-        this.currentSize = 0;
+        this.currentIndex = 0;
     }
 
     public void add(int item) {
-        if (capacity <= currentSize) {
+        if (capacity <= currentIndex) {
             capacity += capacity;
             int[] enlargedArray = new int[capacity];
             System.arraycopy(data, 0, enlargedArray, 0, capacity);
             data = enlargedArray;
         }
-        data[currentSize] = item;
-        currentSize++;
+        data[currentIndex] = item;
+        currentIndex++;
     }
 
     public int size() {
